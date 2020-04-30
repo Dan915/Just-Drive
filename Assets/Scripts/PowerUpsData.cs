3bbdbeeb321431a1d522c21c818c1e1275d2a95d
Dan915 <buffalo9715@gmail.com>
2020-04-30 13:27:43 +0100

    Power ups scriptable object

---
+++ Assets/Scripts/PowerUpsData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "PowerUpsData", menuName = "Custom/PowerUpsData", order = 0)]
+public class PowerUpsData : ScriptableObject {
+
+
+    public new string name;
+    public string description;
+    public string upgrade1Description, upgrade2Description;
+    public Sprite artwork; 
+    public GameObject prefab;
+    public float duration;
+    public float strenght;
+    public int upgrade1Price, upgrade2Price;    
+
+}
--- Assets/Scripts/WorldPieceData.cs
+++ Assets/Scripts/WorldPieceData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "World Piece Data 0", menuName = "World Piece", order = 1)]
+[CreateAssetMenu(fileName = "World Piece Data 0", menuName = "Custom/World Piece", order = 1)]
 public class WorldPieceData : ScriptableObject
 {
     public enum trackName
