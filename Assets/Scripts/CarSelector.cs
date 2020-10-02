bb9316806335aae7ce27932c855d7f26d9357bb4
Daniel <BUFFALO9715@gmail.com>
2020-10-02 18:49:18 +0100

    small improvements

--- Assets/Scripts/CarSelector.cs
+++ Assets/Scripts/CarSelector.cs
@@ -6,6 +6,7 @@ using Michsky.UI.ModernUIPack;
 public class CarSelector : MonoBehaviour
 {
     public CarData[] carData;
+    private CarData currentCarSelected;
     public GameObject player;
     public int price;
     public bool isUnlocked = false;
@@ -15,31 +16,53 @@ public class CarSelector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    public void ChooseCar()
-    {
-        Debug.Log("Choose Car");
         price = carData[0].price;
         isUnlocked = carData[0].isUnlocked;
-
         thumbnail = carData[0].thumbnail;
+        Image.GetComponent<Image>().sprite = thumbnail;
+        mySelector.defaultIndex = 0;
+    }
+
+    public void Van()
+    {
+        int i = 1;
+        Debug.Log("Choose Car");
+        price = carData[i].price;
+        isUnlocked = carData[i].isUnlocked;
+        thumbnail = carData[i].thumbnail;
         Image.GetComponent<Image>().sprite = thumbnail;
         if (isUnlocked)
         {
-            player.GetComponentInChildren<PlayerController>().car = carData[0].prefab;
+            player.GetComponentInChildren<PlayerController>().car = carData[i].prefab;
         }
         else
         {
             Debug.Log("Go get some money");
         }
+        currentCarSelected = carData[i];
      
+    }
+    public void Coupe()
+    {
+        int i = 0;
+        Debug.Log("Choose Coupe");
+        price = carData[i].price;
+        isUnlocked = carData[i].isUnlocked;
+        thumbnail = carData[i].thumbnail;
+        Image.GetComponent<Image>().sprite = thumbnail;
+        if (isUnlocked)
+        player.GetComponentInChildren<PlayerController>().car = carData[i].prefab;
+        else
+            Debug.Log("Get more Money");
+        
+        currentCarSelected = carData[i];
     }
     
     public void BuyCar()
     {
+        Debug.Log("if enough money buy the car");
         isUnlocked = true;
+        currentCarSelected.isUnlocked = true;
     }
 
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,6 +8,7 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
     public int score, highScore, coins, coinValue;
     public TextMeshProUGUI scoreText, highScoreText, coinsText;
+    public WorldGenerator worldGenerator;
 
     private void Awake() 
     {
@@ -19,6 +20,7 @@ public class ScoreManager : MonoBehaviour
         coins+= coinValue;
 
         coinsText.text = coins.ToString();
+        worldGenerator.score = coins;
     }
     public void UpdateScore()
     {
